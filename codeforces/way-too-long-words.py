af1b8d01a29ab10837ef3f0df452b372ec2f8214
Aishani Singh <66542645+aishanisingh@users.noreply.github.com>
2021-06-16 17:12:04 -0700

    better variables

--- codeforces/way-too-long-words.py
+++ codeforces/way-too-long-words.py
@@ -1,24 +1,22 @@
-#https://codeforces.com/problemset/problem/71/A
-
 integer = int(input())
 lines = [input() for i in range(integer)]
 
 for word in lines:
-  lockers_list=[]
-  lockers_list[:0] = word
+  lists=[]
+  lists[:0] = word
   myarr = []
-  if len(lockers_list) > 10:
-    first = lockers_list[0]
-    last = lockers_list[-1]
-    lockers_list.pop(0)
-    lockers_list.pop(-1)
-    length = len(lockers_list)
+  if len(lists) > 10:
+    first = lists[0]
+    last = lists[-1]
+    lists.pop(0)
+    lists.pop(-1)
+    length = len(lists)
     myarr.append(first)
     myarr.append(str(length))
     myarr.append(last)
     x = ' '.join(myarr)
     print(x.replace(" ", ""))
-  elif len(lockers_list) <= 10:
+  elif len(lists) <= 10:
     print(word)
 
 
