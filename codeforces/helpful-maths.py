11145df576079b663c396b87969934c01c7a76f7
Aishani Singh <66542645+aishanisingh@users.noreply.github.com>
2021-06-17 10:26:35 -0700

    created

---
+++ codeforces/helpful-maths.py
@@ -0,0 +1,38 @@
+#https://codeforces.com/problemset/problem/339/A
+#TODO: clean up this code because it's prettu redundant and you could do better
+
+inputs = input()
+
+myarr0 = []
+myarr0[:0] = inputs
+
+number_list = [x for x in myarr0 if x != '+']
+number_list = [int(i) for i in number_list]
+
+list_size = len(number_list)
+
+for j in range(list_size):
+  smallest_index = j
+  smallest = number_list[j]
+  for i in range(j+1,list_size):
+    if smallest > number_list[i]:
+      smallest = number_list[i]
+      smallest_index = i
+  if smallest_index != j:
+    temp = number_list[j]
+    number_list[j] = smallest 
+    number_list[smallest_index] = temp 
+
+myarr = []
+for i in number_list:
+  myarr.append(str(i))
+
+string = ' '.join(myarr)
+
+myarr3 = []
+myarr3[:0] = string
+
+newlist = ['+' if x==' ' else x for x in myarr3]
+newerlist = ''.join(newlist)
+
+print(newerlist)
