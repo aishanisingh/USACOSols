c6ffd9af87b1b71003c702e1bd86599d3fd2a704
Aishani Singh <66542645+aishanisingh@users.noreply.github.com>
2021-06-22 19:52:36 -0700

    created COWPHABET

---
+++ usaco/2021/january/bronze/cowphabet.py
@@ -0,0 +1,22 @@
+#i hate this problem. so much.
+
+alphabet = input()
+s = input()
+
+reverse = {}
+
+for i in range(len(alphabet)):
+  reverse[alphabet[i]] = i
+
+print(reverse)
+
+curr = -1
+count = 1
+
+for c in s:
+  print(c)
+  if reverse[c] <= curr:
+    count+=1
+  curr = reverse[c]
+
+print(count)
